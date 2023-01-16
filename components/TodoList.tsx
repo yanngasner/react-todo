43759eb9322ea46ec29954b8550c9cae22caa9cd
Yann Gasner <yann.gasner@gmail.com>
2023-01-16 21:24:45 +0100

    style wip

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -5,6 +5,8 @@ import { useListActions } from "@/services/useListActions";
 import { useState } from "react";
 import { isTemplateExpression } from "typescript";
 import TodoItem from "./TodoItem";
+import styles from '@/styles/TodoList.module.css';
+
 
 const TodoList: React.FC<TodoListProps> = ({ todoList, updateList, deleteList }) => {
 
@@ -13,25 +15,27 @@ const TodoList: React.FC<TodoListProps> = ({ todoList, updateList, deleteList })
         = useListActions({ todoList, updateList, deleteList });
 
     return (
-        <>
-            <div>{todoList.id}</div>
-            <div>{todoList.name}</div>
-            <div>{todoList.description}</div>
-            {todoList.todoItems.map(item =>
-                <TodoItem
-                    key={item.id}
-                    todoItem={item}
-                    updateItem={updateItem}
-                    deleteItem={deleteItem}
-                />)
-            }
-            <div></div>
-            <button onClick={() => onDeleteListClicked}>Supprimer</button>
-            <button onClick={onClearListClicked}>Nettoyer</button>
-            <input onChange={e => setItemName(e.target.value)} />
-            <button onClick={() => onAddItemClicked(itemName)}>Add</button>
+        <div className={styles.container}>
+            <div className={styles.todoList}>
+                <div className={styles.head}>
+                    <h1>{todoList.name}</h1>
+                    <h2>{todoList.description}</h2>
+                </div>
+                {todoList.todoItems.map(item =>
+                    <TodoItem
+                        key={item.id}
+                        todoItem={item}
+                        updateItem={updateItem}
+                        deleteItem={deleteItem}
+                    />)
+                }
+                <button onClick={() => onDeleteListClicked}>Supprimer</button>
+                <button onClick={onClearListClicked}>Nettoyer</button>
+                <input onChange={e => setItemName(e.target.value)} />
+                <button onClick={() => onAddItemClicked(itemName)}>Add</button>
+            </div>
+        </div>
 
-        </>
     );
 }
 
--- components/TodoListCard.tsx
+++ components/TodoListCard.tsx
@@ -1,19 +1,17 @@
 
 import { TodoListModel } from '@/models/TodoListModel';
-import styles from '@/styles/TodoListCard.module.css'
-import { Inter } from '@next/font/google'
+import styles from '@/styles/TodoListCard.module.css';
 import Router from "next/router";
 
-const inter = Inter({ subsets: ['latin'] })
 
 const TodoListCard: React.FC<{ todoList: TodoListModel }> = ({ todoList }) => {
     return (
         <>
             <div onClick={() => Router.push("/todoList/[id]", `/todoList/${todoList.id}`)} className={`${styles.card} clickable`}>
-                <h2 className={inter.className}>
+                <h2>
                     {todoList.name} <span>-&gt;</span>
                 </h2>
-                <p className={inter.className}>
+                <p>
                     {todoList.description}
                 </p>
             </div>
